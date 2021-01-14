0735fb395b2b4cce412b8da3119bfa666d4873fb
Katherine Prudnik <katwaterfall7@gmail.com>
2021-01-14 23:38:59 +0300

    feat: add possibility to mark tasks

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,61 @@
-let listItems = document.getElementsByTagName("LI");
-let i;
-for (i = 0; i < listItems.length; i++) {
+function addCloseBtn() {
     let closeButton = document.createElement("SPAN");
     let txt = document.createTextNode("\u00D7");
     closeButton.className = "closeButton";
     closeButton.append(txt);
-    listItems[i].append(closeButton);
+    return closeButton;
+}
+function deleteListItem() {//замечание:если обрабатываются нажатия на список элементов, лучше-event.target
+    let list = document.getElementById("list");
+    list.addEventListener('click', function (event) {//ul-т.к. будет отслеживание вложенных элементов
+        if (event.target.className === "closeButton") {//именно "крестик"
+            event.target.parentElement.style.display = "none";
+        }
+    }, false);
+}
+function addListItem() {
+    let addButton = document.getElementById("addButton");
+    let input = document.getElementById("input");
+    let list = document.getElementById("list");
+    addButton.addEventListener('click', function () {
+        if (input.value === "") {
+            alert("You should fill the field");
+        }
+        else {
+            let newListItem = document.createElement("LI");
+            newListItem.innerHTML = input.value;
+            list.prepend(newListItem);
+            input.value = "";
+
+            let closeButton = addCloseBtn();
+            newListItem.append(closeButton);       
+        }
+    }, false);
+}
+function addCloseBtnToExistingList() {
+    let listItems = document.getElementsByTagName("LI");
+    let i;
+    for (i = 0; i < listItems.length; i++) {
+        let closeButton = addCloseBtn();
+        listItems[i].append(closeButton);
+    }
+}
+function markCompletedTask() {
+    let list = document.getElementById("list");
+    list.addEventListener('click', function (event) {//ul-т.к. будет отслеживание вложенных элементов
+        // listItems[i].classList.toggle("marked"); так нельзя, событие не обрабатывается, li считается неопределенным
+        if (event.target.tagName === "LI") {//именно элемент списка
+            event.target.classList.toggle("marked");//естьь такой класс-удаляет как remove, нет-добавляет как add
+        }
+    }, false);
 }
 
-let closeButtonList = document.getElementsByClassName("closeButton");
-for (i = 0; i < closeButtonList.length; i++) {
-    closeButtonList[i].addEventListener('click', function () {
-        this.parentElement.style.display = "none";
-    }, false)
-}
+addListItem();
+addCloseBtnToExistingList();
+deleteListItem();
+markCompletedTask();
 
-let addButton = document.getElementById("addButton");
-let input = document.getElementById("input");
-let list = document.getElementById("list");
-addButton.addEventListener('click', function () {
-    if(input.value === ""){
-        alert("You should fill the field");
-    }
-    else{
-        let newListItem = document.createElement("LI");
-        newListItem.innerHTML = input.value;
-        list.prepend(newListItem);
-        input.value = "";
-    }
-}, false);
+
+
+
 
