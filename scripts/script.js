a14794e80ae6d154d76054bf3855a57956e255e4
Katherine Prudnik <katwaterfall7@gmail.com>
2021-01-19 22:00:03 +0300

    feat: add import functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,75 @@
+function importFile() {
+
+    let importInput = document.getElementById("import");
+    importInput.addEventListener('change', function () {
+        let file = importInput.files[0];// сам файл
+        let reader = new FileReader();
+        reader.readAsText(file); //для получения строки
+        //события
+        reader.onload = function () { //файл загружен
+            console.log(reader.result);
+            let toDoList = getToDoItemsObjects(reader);
+            console.log(JSON.stringify(toDoList));
+            handleTasksListBox(toDoList);
+        };
+
+        reader.onerror = function () {
+            console.log(reader.error);
+        };
+
+    }, false)
+
+}
+function getToDoItemsObjects(reader) {
+    let toDoList = [];
+    let fileString;
+    fileString = reader.result;
+    let stringArray = fileString.split('\n'); //массив из строк файла
+    let properties = stringArray[0].split(',');
+    for (let i = 1; i < stringArray.length; i++) {
+        let values = stringArray[i].split(',');
+        let toDoItem = {};
+        for (let j = 0; j < properties.length; j++) {
+            toDoItem[properties[j].trim()] = values[j].trim();
+        }
+        toDoList.push(toDoItem); //массив объектов
+    }
+    return toDoList;
+}
+function handleTasksListBox(toDoList) {
+    let main = document.getElementsByClassName("main")[0];
+    let tasksListBox = document.createElement("DIV");
+    tasksListBox.className = "tasksListBlock";
+    let ul = document.createElement("UL");
+    ul.id = "list";
+    for (let i = 0; i < toDoList.length; i++) {
+        let li = document.createElement("LI");
+        li.innerHTML = toDoList[toDoList.length-1-i].text;
+        ul.prepend(li);
+    }
+    tasksListBox.append(ul);
+    main.append(tasksListBox);
+
+    addCloseBtnToExistingList();
+    deleteListItem();
+    addListItem();
+    markCompletedTask();
+    switchListItemsState();
+}
 function addCloseBtn() {
     let closeButton = document.createElement("SPAN");
     let txt = document.createTextNode("\u00D7");
     closeButton.className = "closeButton";
     closeButton.append(txt);
     return closeButton;
+}
+function addCloseBtnToExistingList() {
+    let listItems = document.getElementsByTagName("LI");
+    let i;
+    for (i = 0; i < listItems.length; i++) {
+        let closeButton = addCloseBtn();
+        listItems[i].append(closeButton);
+    }
 }
 function deleteListItem() {//замечание:если обрабатываются нажатия на список элементов, лучше-event.target
     let list = document.getElementById("list");
@@ -34,14 +100,6 @@ function addListItem() {
         }
     }, false);
 }
-function addCloseBtnToExistingList() {
-    let listItems = document.getElementsByTagName("LI");
-    let i;
-    for (i = 0; i < listItems.length; i++) {
-        let closeButton = addCloseBtn();
-        listItems[i].append(closeButton);
-    }
-}
 function markCompletedTask() {
     let list = document.getElementById("list");
     list.addEventListener('click', function (event) {//ul-т.к. будет отслеживание вложенных элементов
@@ -68,11 +126,8 @@ function switchListItemsState() {
     })
 }
 
-addListItem();
-addCloseBtnToExistingList();
-deleteListItem();
-markCompletedTask();
-switchListItemsState();
+importFile();
+
 
 
 
